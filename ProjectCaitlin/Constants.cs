756b3121287a86c81813eb66d00c8b345a9e82e9
Kyle Hoefer Laptop <kylelaptop@Kyles-MacBook-Pro-3.local>
2020-01-28 02:35:58 -0800

    Added Correct ClientID's

--- ProjectCaitlin/Constants.cs
+++ ProjectCaitlin/Constants.cs
@@ -5,7 +5,7 @@
         public static string AppName = "Project Caitlin";
 
         public static string iOSClientId = "1009120542229-m56ni4pvg8pj4tffuc2i6pthevrken6m.apps.googleusercontent.com";
-        public static string AndroidClientId = "1009120542229-jg6aagdn6ut0d287n621v7te6kjjb4nk.apps.googleusercontent.com";
+        public static string AndroidClientId = "1009120542229-61dahtq9jolmhmf72b4v3boeehfjrs08.apps.googleusercontent.com";
 
         public static string Scope = "https://www.googleapis.com/auth/calendar";
         public static string AuthorizeUrl = "https://accounts.google.com/o/oauth2/auth";
@@ -13,7 +13,7 @@
         public static string UserInfoUrl = "https://www.googleapis.com/oauth2/v2/userinfo";
 
         public static string iOSRedirectUrl = "com.googleusercontent.apps.1009120542229-m56ni4pvg8pj4tffuc2i6pthevrken6m:/oauth2redirect";
-        public static string AndroidRedirectUrl = "urn:ietf:wg:oauth:2.0:oob";
+        public static string AndroidRedirectUrl = "com.googleusercontent.apps.1009120542229-61dahtq9jolmhmf72b4v3boeehfjrs08:/oauth2redirect";
 
     }
 }
