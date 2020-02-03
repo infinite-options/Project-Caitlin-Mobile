41ca5bd8a17d1bcf15c36d3cdc7bf814a08f9304
TyrantIII <r573049226@gmail.com>
2020-02-03 14:20:55 -0800

    -

--- ProjectCaitlin/Authentication/GoogleOAuthToken.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProjectCaitlin.Authentication
-{
-    public class GoogleOAuthToken
-    {
-        public string TokenType { get; set; }
-        public string AccessToken { get; set; }
-    }
-}
--- ProjectCaitlin/AuthenticationState.cs
+++
@@ -1,9 +0,0 @@
-﻿using Xamarin.Auth;
-
-namespace ProjectCaitlin
-{
-    public class AuthenticationState
-    {
-        public static OAuth2Authenticator Authenticator;
-    }
-}
