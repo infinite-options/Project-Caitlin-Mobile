89e4a719156d064e125f145516e2711c0b4ec831
Jeremy Manalo <jeremy.h.manalo@gmail.com>
2020-04-28 23:45:46 -0700

    fix has been made

--- ProjectCaitlin.iOS/Properties/AssemblyInfo.cs
+++ ProjectCaitlin.iOS/Properties/AssemblyInfo.cs
@@ -15,9 +15,6 @@ using Xamarin.Forms;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// For depricated iOS Web API
-[assembly: ExportRenderer(typeof(Xamarin.Forms.WebView), typeof(Xamarin.Forms.Platform.iOS.WebViewRenderer))]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
