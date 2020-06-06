69a2863c2f3676c9f0eaf1c139d5dcc134e5f533
Jeremy Manalo <jeremy.h.manalo@gmail.com>
2020-06-06 03:53:47 -0700

    new branch for watch app test

---
+++ ProjectCaitlinWatchApp/ProjectCaitlinWatchApp.WatchOSExtension/ExtensionDelegate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Foundation;
+using WatchKit;
+
+namespace ProjectCaitlinWatchApp.WatchOSExtension
+{
+    public class ExtensionDelegate : WKExtensionDelegate
+    {
+        public ExtensionDelegate()
+        {
+        }
+    }
+}
+
