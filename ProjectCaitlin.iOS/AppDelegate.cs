3f264de142cb4c2803fa1196130d7090d41f647d
Hardik Kumar <55339320+hk9984@users.noreply.github.com>
2020-08-04 20:19:17 -0700

    iOS push notif

--- ProjectCaitlin.iOS/AppDelegate.cs
+++ ProjectCaitlin.iOS/AppDelegate.cs
@@ -7,6 +7,7 @@ using Foundation;
 using PanCardView.iOS;
 using UIKit;
 using UserNotifications;
+using Firebase.CloudMessaging;
 
 namespace ProjectCaitlin.iOS
 {
@@ -14,7 +15,7 @@ namespace ProjectCaitlin.iOS
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, IUNUserNotificationCenterDelegate
     {
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
@@ -40,8 +41,91 @@ namespace ProjectCaitlin.iOS
 
             global::Xamarin.Auth.Presenters.XamarinIOS.AuthenticationConfiguration.Init();
 
+            // get permission for notification
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                // iOS 10
+                var authOptions = UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
+                UNUserNotificationCenter.Current.RequestAuthorization(authOptions, (granted, error) =>
+                {
+                    Console.WriteLine("whether granted: " + granted);
+                    if (granted)
+                    {
+                        InvokeOnMainThread(() =>
+                        {
+                            UIApplication.SharedApplication.RegisterForRemoteNotifications();
+                        });
+                    }
+                });
+
+                // For iOS 10 display notification (sent via APNS)
+                UNUserNotificationCenter.Current.Delegate = this;
+            }
+            else
+            {
+                // iOS 9 <=
+                var allNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound;
+                var settings = UIUserNotificationSettings.GetSettingsForTypes(allNotificationTypes, null);
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+            }
+
+            UIApplication.SharedApplication.RegisterForRemoteNotifications();
+
+            // Firebase component initialize
+            //Firebase.Analytics.App.Configure();
+
+            Firebase.InstanceID.InstanceId.Notifications.ObserveTokenRefresh((sender, e) => {
+                var newToken = Messaging.SharedInstance.FcmToken;
+                // if you want to send notification per user, use this token
+                App.deviceToken = newToken;
+                App.sendDeviceToken();
+                System.Diagnostics.Debug.WriteLine(newToken);
+
+                //connectFCM();
+            });
+
             return base.FinishedLaunching(app, options);
         }
+
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+        {
+            App.deviceToken = deviceToken.ToString();
+            App.sendDeviceToken();
+        }
+
+        public override void ReceivedRemoteNotification(UIApplication application, NSDictionary userInfo)
+        {
+            ProcessNotification(userInfo, false);
+        }
+
+        void ProcessNotification(NSDictionary options, bool fromFinishedLaunching)
+        {
+            // make sure we have a payload
+            if (options != null && options.ContainsKey(new NSString("aps")))
+            {
+                // get the APS dictionary and extract message payload. Message JSON will be converted
+                // into a NSDictionary so more complex payloads may require more processing
+                NSDictionary aps = options.ObjectForKey(new NSString("aps")) as NSDictionary;
+                string payload = string.Empty;
+                NSString payloadKey = new NSString("data");
+                if (aps.ContainsKey(payloadKey))
+                {
+                    payload = aps[payloadKey].ToString();
+                }
+
+                if (!string.IsNullOrWhiteSpace(payload))
+                {
+                    //(App.Current.MainPage as MainPage)?.AddMessage(payload);
+                    Console.WriteLine(payload);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Received request to process notification but there was no payload.");
+            }
+        }
+
 
         public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
         {
--- ProjectCaitlin/Views/GoalsRoutinesTemplate.xaml.cs
+++ ProjectCaitlin/Views/GoalsRoutinesTemplate.xaml.cs
@@ -11,6 +11,9 @@ namespace ProjectCaitlin.Views
     {
         readonly GoalsRoutinesTemplateViewModel pageModel;
 
+        FirestoreService firestoreService;
+        GoogleService googleService = new GoogleService();
+
         public GoalsRoutinesTemplate()
         {
             InitializeComponent();
@@ -19,6 +22,7 @@ namespace ProjectCaitlin.Views
             pageModel = new GoalsRoutinesTemplateViewModel(this);
             BindingContext = pageModel;
             ContentStackLayout.HeightRequest = Application.Current.MainPage.Height - NavBar.Height;
+            firestoreService = new FirestoreService();
         }
 
         private void AddTapGestures()
@@ -51,6 +55,8 @@ namespace ProjectCaitlin.Views
             var tapGestureRecognizer4 = new TapGestureRecognizer();
             tapGestureRecognizer4.Tapped += async (s, e) => {
                 UserDialogs.Instance.ShowLoading("Refreshing Page...");
+                //await firestoreService.LoadDatabase();
+                //await googleService.LoadTodaysEvents();
                 Navigation.PushAsync(new GoalsRoutinesTemplate());
                 UserDialogs.Instance.HideLoading();
             };
