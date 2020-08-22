82c36dc14550b0a651696ac9d9d5d6c61b03018a
ashutosh <ashutoshmahala@live.com>
2020-08-21 17:19:23 -0700

    BugFix(TimeSettings) Missing File added

---
+++ ProjectCaitlin/Models/TimeSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ProjectCaitlin.Models
+{
+    public class TimeSettings: INotifyPropertyChanged
+    {
+        public string afternoon { get; set; }
+        public string dayEnd { get; set; }
+        public string dayStart { get; set; }
+        public string evening { get; set; }
+        public string morning { get; set; }
+        public string night { get; set; }
+        public string timeZone { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
