741b9b4498d61715143ba63f6b2770f0818fbcc5
ashutosh <ashutoshmahala@live.com>
2020-08-31 16:23:23 -0700

    Feature(Today List Page Dynamic Template Change): added support for change of template dynamically for gratis objects

---
+++ ProjectCaitlin/Controls/GratisTodaysListDataSelector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using ProjectCaitlin.Models;
+
+namespace ProjectCaitlin.Controls
+{
+    public class GratisTodaysListDataSelector : Xamarin.Forms.DataTemplateSelector
+    {
+
+        public DataTemplate EventTodaysListTemplate { get; set; }
+
+        public DataTemplate GoalTodaysListCompleteTemplate { get; set; }
+        public DataTemplate GoalTodaysListNotCompleteTemplate { get; set; }
+        
+        public DataTemplate RoutineTodaysListCompleteTemplate { get; set; }
+        public DataTemplate RoutineTodaysListNotCompleteTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            if (item.GetType() == typeof(goal))
+            {
+                return ((item as goal).isComplete) ? GoalTodaysListCompleteTemplate : GoalTodaysListNotCompleteTemplate;
+            }
+            else if (item.GetType() == typeof(routine))
+            {
+                return ((item as routine).isComplete) ? RoutineTodaysListCompleteTemplate : RoutineTodaysListNotCompleteTemplate;
+            }
+            else
+            {
+                return EventTodaysListTemplate;
+            }
+        }
+    }
+}
